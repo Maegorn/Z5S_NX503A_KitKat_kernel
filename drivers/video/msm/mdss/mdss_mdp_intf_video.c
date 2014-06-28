9577c7529f39036fa5f7055defc167fa848bbd86
Jayant Shekhar <jshekhar@codeaurora.org>
2014-06-28 12:35:30 +0530

    msm: mdss: Wait for vsync before iommu attach during cont splash
    During first borderfill commit in splash cleanup, wait for vsync
    before iommu attach to prevent IOMMU page faults during bootup.
    
    Change-Id: I68b7162d090513b082cbd4c0013731cedc0ebdbe
    Signed-off-by: Jayant Shekhar <jshekhar@codeaurora.org>
    Signed-off-by: Vineet Bajaj <vbajaj@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_intf_video.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_video.c
@@ -668,6 +668,7 @@ static int mdss_mdp_video_config_fps(struct mdss_mdp_ctl *ctl,
 static int mdss_mdp_video_display(struct mdss_mdp_ctl *ctl, void *arg)
 {
 	struct mdss_mdp_video_ctx *ctx;
+	struct mdss_panel_data *pdata = ctl->panel_data;
 	int rc;
 
 	pr_debug("kickoff ctl=%d\n", ctl->num);
@@ -699,6 +700,11 @@ static int mdss_mdp_video_display(struct mdss_mdp_ctl *ctl, void *arg)
 		}
 
 		pr_debug("enabling timing gen for intf=%d\n", ctl->intf_num);
+
+		if (pdata->panel_info.cont_splash_enabled) {
+			rc = wait_for_completion_timeout(&ctx->vsync_comp,
+					usecs_to_jiffies(VSYNC_TIMEOUT_US));
+		}
 
 		mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_ON, false);
 
